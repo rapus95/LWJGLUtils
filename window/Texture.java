76150715d593c1b13c9302aa8be7f82fa6ec8b3d
XOR <nils.h.emmerich@gmail.com>
2015-05-22 22:49:15 +0200

    now working

--- window/Texture.java
+++ window/Texture.java
@@ -8,6 +8,8 @@ import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map.Entry;
 
+import javax.imageio.ImageIO;
+
 import org.lwjgl.opengl.GL11;
 
 import de.matthiasmann.twl.utils.PNGDecoder;
@@ -20,6 +22,7 @@ public class Texture {
 	public final int textureHandle;
 	
 	public static Texture create(Path path){
+		System.out.println("NOW");
 		Format f = PNGDecoder.Format.RGBA;
 		try(InputStream s = Files.newInputStream(path)){
 			for(Entry<Path, Texture> e:map.entrySet()){
@@ -35,7 +38,8 @@ public class Texture {
 			buf.flip();
 			int texture;
 			texture = GL11.glGenTextures();
-			GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGB, decoder.getWidth(), decoder.getHeight(), 0, GL11.GL_RGB, GL11.GL_UNSIGNED_BYTE, buf);
+			GL11.glBindTexture(GL11.GL_TEXTURE_2D, texture);
+			GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGB, decoder.getWidth(), decoder.getHeight(), 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, buf);
 			GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_NEAREST);
 			GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST);
 			Texture t = new Texture(texture);
